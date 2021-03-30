b91785e0e499b88a0d6378ebb9d6dee7ff522b77
Lingfeng Yang <lfy@google.com>
2021-03-30 14:52:11 -0700

    Add more tests and update README

--- README.md
+++ README.md
@@ -25,4 +25,135 @@ The shared region has the following layout:
 
 # TODO
 
-The code right now is quite roughly extracted from https://android.googlesource.com/device/generic/goldfish-opengl and https://android.googlesource.com/device/generic/vulkan-cereal, and needs generic tests that aren't gfxstream specific.
+The code right now is quite roughly extracted from https://android.googlesource.com/device/generic/goldfish-opengl and https://android.googlesource.com/device/generic/vulkan-cereal, and needs:
+
+- Performance tests
+- Maybe provide some plausible host IPC implementations of doorbell callbacks and shared memory?
+
+# How to use
+
+See unit tests in `test/asg_unittest.cpp` for more details. A walkthrough of the initialization in the `Basic` test:
+
+## Define constants and initialize shared buffer with ring fields
+
+First, we define constants that say how large the xfer buffer is going to be:
+
+```
+    static constexpr size_t kRingXferSize = 16384;
+```
+
+The total size occupied by the shared buffer will be `sizeof(struct asg_ring_storage + kRingXferSize`. Next, we define a step size for flushing buffers on the ring. It must be a power of 2 that is less than half `kRingXferSize`:
+
+```
+    static constexpr size_t kRingStepSize = 4096;
+```
+
+Then, the shared buffer is initialized accordingly:
+
+```
+    std::vector<uint8_t> sharedBuf(sizeof(struct asg_ring_storage) + kRingXferSize, 0);
+    uint8_t* sharedBufPtr = sharedBuf.data();
+
+    struct asg_context context =
+        asg_context_create((char*)sharedBufPtr, (char*)sharedBufPtr + sizeof(struct asg_ring_storage), kRingXferSize);
+
+    context.ring_config->buffer_size = kRingXferSize;
+    context.ring_config->flush_interval = kRingStepSize;
+    context.ring_config->host_consumed_pos = 0;
+    context.ring_config->transfer_mode = 1;
+    context.ring_config->in_error = 0;
+```
+
+`asg_context_create()` along with the following `ring_config` field settings populate the `asg_ring_storage` section of the shared memory with the correct struct layout and other parameters, and also initializes fields for the ring buffers themselves.
+
+    MessageChannel<int, 1> doorbellChannel;
+    bool stop = false;
+
+    auto doorbell = [&doorbellChannel]() {
+        fprintf(stderr, "%s: doorbell\n", __func__);
+        doorbellChannel.trySend(0);
+    };
+
+    auto unavailRead = [&doorbellChannel, &stop]() {
+        int item;
+        fprintf(stderr, "%s: unavailable, wait\n", __func__);
+        doorbellChannel.receive(&item);
+        fprintf(stderr, "%s: unavailable, got a doorbell\n", __func__);
+        if (stop) return -1;
+        return 0;
+    };
+
+    asg::client::RingStream clientStream(sharedBufPtr, kRingXferSize, doorbell);
+    asg::server::RingStream serverStream(sharedBufPtr, kRingXferSize, unavailRead);
+```
+
+## Define doorbell callbacks on the client and server
+
+ASG requires a mechanism for the client to wake the server, and for the server to receive such wakeup requests, on a channel that isn't necessarily reliant on shared memory. There are two sides to this:
+    - Client: doorbell callback called whenever the client, according to AGS ring protocol, thinks it might be a good idea to wake up the server.
+    - Server: unavailable read callback called when we want to stop listening for traffic from the client and go to sleep inside this callback. It is assumed the client doorbell callback will trigger stuff that wakes up the server from the point in the callback where it's sleeping.
+
+Note that these callbacks can be no-ops if the server spins forever. But usually we don't want that, so we have the server go to sleep in its unavailable-read callback.
+
+```
+    MessageChannel<int, 1> doorbellChannel;
+    bool stop = false;
+
+    auto doorbell = [&doorbellChannel]() {
+        fprintf(stderr, "%s: doorbell\n", __func__);
+        doorbellChannel.trySend(0);
+    };
+
+    auto unavailRead = [&doorbellChannel, &stop]() {
+        int item;
+        fprintf(stderr, "%s: unavailable, wait\n", __func__);
+        doorbellChannel.receive(&item);
+        fprintf(stderr, "%s: unavailable, got a doorbell\n", __func__);
+        if (stop) return -1;
+        return 0;
+    };
+```
+
+In the unit tests, we implement this with a message channel. The server side blocks in `MessageChannel::receive`, to be woke up by the `doorbell` function defined above it.
+
+## Initialize client/server RingStreams and start sending traffic
+
+```
+    asg::client::RingStream clientStream(sharedBufPtr, kRingXferSize, doorbell);
+    asg::server::RingStream serverStream(sharedBufPtr, kRingXferSize, unavailRead);
+```
+
+After initializing, we can then send/receive traffic. In the test, it's realized as starting two threads, one for client and one for server, and sending traffic like so:
+
+```
+    FunctorThread clientTestThread([&clientStream]() {
+        for (uint32_t i = 0; i < kSends; ++i) {
+            auto buf = clientStream.alloc(kSendSizeBytes);
+            memset(buf, 0xff, kSendSizeBytes);
+            fprintf(stderr, "%s: sent packet %d\n", __func__, i);
+        }
+        clientStream.flush();
+    });
+
+    FunctorThread serverTestThread([&serverStream]() {
+        std::vector<uint8_t> readBuf(kSendSizeBytes);
+        for (uint32_t i = 0; i < kSends; ++i) {
+            size_t wanted = kSendSizeBytes;
+            size_t read = 0;
+            while (read < wanted) {
+                read += serverStream.read(readBuf.data() + read, wanted - read);
+            }
+            fprintf(stderr, "%s: received packet %u\n", __func__, i);
+        }
+    });
+
+    serverTestThread.start();
+    clientTestThread.start();
+
+    clientTestThread.wait();
+    serverTestThread.wait();
+```
+
+Note the use of `alloc` only, instead of always `alloc` followed by `flush`. This can save on doorbells, but even if we always follow up flush from alloc, it's possible to still avoid doorbells in that case.
+
+The tests contain further code that demonstrates sending replies. There is also a random test to test correctness more thoroughly.
--- tests/asg_unittest.cpp
+++ tests/asg_unittest.cpp
@@ -10,6 +10,7 @@
 #include <inttypes.h>
 
 #include <functional>
+#include <random>
 #include <vector>
 
 using android::base::MessageChannel;
@@ -68,9 +69,196 @@ TEST(ASG, Basic) {
             size_t wanted = kSendSizeBytes;
             size_t read = 0;
             while (read < wanted) {
-                read = serverStream.read(readBuf.data(), wanted - read);
+                read += serverStream.read(readBuf.data() + read, wanted - read);
             }
             fprintf(stderr, "%s: received packet %u\n", __func__, i);
+        }
+    });
+
+    serverTestThread.start();
+    clientTestThread.start();
+
+    clientTestThread.wait();
+    serverTestThread.wait();
+}
+
+TEST(ASG, BasicRoundTrip) {
+    static constexpr size_t kRingXferSize = 16384;
+    static constexpr size_t kRingStepSize = 4096;
+    static constexpr size_t kRoundTrips = 1024;
+    static constexpr size_t kSendSizeBytes = 384;
+
+    std::vector<uint8_t> sharedBuf(sizeof(struct asg_ring_storage) + kRingXferSize, 0);
+    uint8_t* sharedBufPtr = sharedBuf.data();
+
+    struct asg_context context =
+        asg_context_create((char*)sharedBufPtr, (char*)sharedBufPtr + sizeof(struct asg_ring_storage), kRingXferSize);
+
+    context.ring_config->buffer_size = kRingXferSize;
+    context.ring_config->flush_interval = kRingStepSize;
+    context.ring_config->host_consumed_pos = 0;
+    context.ring_config->transfer_mode = 1;
+    context.ring_config->in_error = 0;
+
+    MessageChannel<int, 1> doorbellChannel;
+    bool stop = false;
+
+    auto doorbell = [&doorbellChannel]() {
+        fprintf(stderr, "%s: doorbell\n", __func__);
+        doorbellChannel.trySend(0);
+    };
+
+    auto unavailRead = [&doorbellChannel, &stop]() {
+        int item;
+        fprintf(stderr, "%s: unavailable, wait\n", __func__);
+        doorbellChannel.receive(&item);
+        fprintf(stderr, "%s: unavailable, got a doorbell\n", __func__);
+        if (stop) return -1;
+        return 0;
+    };
+
+    asg::client::RingStream clientStream(sharedBufPtr, kRingXferSize, doorbell);
+    asg::server::RingStream serverStream(sharedBufPtr, kRingXferSize, unavailRead);
+
+    FunctorThread clientTestThread([&clientStream]() {
+        std::vector<uint8_t> readBuf(kSendSizeBytes);
+        for (uint32_t i = 0; i < kRoundTrips; ++i) {
+            auto buf = clientStream.alloc(kSendSizeBytes);
+            memset(buf, 0xff, kSendSizeBytes);
+            fprintf(stderr, "%s: sent packet %d\n", __func__, i);
+            clientStream.readback(readBuf.data(), kSendSizeBytes);
+            fprintf(stderr, "%s: read back packet %d\n", __func__, i);
+        }
+    });
+
+    FunctorThread serverTestThread([&serverStream]() {
+        std::vector<uint8_t> readBuf(kSendSizeBytes);
+        for (uint32_t i = 0; i < kRoundTrips; ++i) {
+            size_t wanted = kSendSizeBytes;
+            size_t read = 0;
+            while (read < wanted) {
+                read += serverStream.read(readBuf.data() + read, wanted - read);
+            }
+            fprintf(stderr, "%s: received packet %u\n", __func__, i);
+            serverStream.writeFully(readBuf.data(), kSendSizeBytes);
+            fprintf(stderr, "%s: wrote back packet %u\n", __func__, i);
+        }
+    });
+
+    serverTestThread.start();
+    clientTestThread.start();
+
+    clientTestThread.wait();
+    serverTestThread.wait();
+}
+
+TEST(ASG, RandomTraffic) {
+    static constexpr size_t kRingXferSize = 16384;
+    static constexpr size_t kRingStepSize = 4096;
+    static constexpr size_t kTraffics = 1024;
+
+    enum TrafficType {
+        Write = 0,
+        Read = 1,
+    };
+
+    struct Traffic {
+        TrafficType type;
+        uint32_t size;
+        uint8_t byteVal;
+    };
+
+    std::vector<Traffic> traffics;
+
+    std::default_random_engine gen;
+    gen.seed(0);
+    std::bernoulli_distribution readProbDist(0.01);
+    std::uniform_int_distribution<uint32_t> trafficSizeDist(1, 8190);
+    std::uniform_int_distribution<uint8_t> byteValDist(0, 255);
+
+    for (uint32_t i = 0; i < kTraffics; ++i) {
+        TrafficType type = readProbDist(gen) ? TrafficType::Read : TrafficType::Write;
+        uint32_t size = trafficSizeDist(gen);
+        uint8_t byteVal = byteValDist(gen);
+        traffics.push_back({ type, size, byteVal });
+    }
+
+    std::vector<uint8_t> sharedBuf(sizeof(struct asg_ring_storage) + kRingXferSize, 0);
+    uint8_t* sharedBufPtr = sharedBuf.data();
+
+    struct asg_context context =
+        asg_context_create((char*)sharedBufPtr, (char*)sharedBufPtr + sizeof(struct asg_ring_storage), kRingXferSize);
+
+    context.ring_config->buffer_size = kRingXferSize;
+    context.ring_config->flush_interval = kRingStepSize;
+    context.ring_config->host_consumed_pos = 0;
+    context.ring_config->transfer_mode = 1;
+    context.ring_config->in_error = 0;
+
+    MessageChannel<int, 1> doorbellChannel;
+    bool stop = false;
+
+    auto doorbell = [&doorbellChannel]() {
+        doorbellChannel.trySend(0);
+    };
+
+    auto unavailRead = [&doorbellChannel, &stop]() {
+        int item;
+        doorbellChannel.receive(&item);
+        if (stop) return -1;
+        return 0;
+    };
+
+    asg::client::RingStream clientStream(sharedBufPtr, kRingXferSize, doorbell);
+    asg::server::RingStream serverStream(sharedBufPtr, kRingXferSize, unavailRead);
+
+    FunctorThread clientTestThread([&clientStream, &traffics]() {
+        std::vector<uint8_t> readBuf;
+        std::vector<uint8_t> expectedReadBuf;
+        uint8_t* sendBuf;
+        for (uint32_t i = 0; i < kTraffics; ++i) {
+            switch (traffics[i].type) {
+            case TrafficType::Write:
+                sendBuf = clientStream.alloc(traffics[i].size);
+                memset(sendBuf, traffics[i].byteVal, traffics[i].size);
+                break;
+            case TrafficType::Read:
+                readBuf.resize(traffics[i].size);
+                expectedReadBuf.resize(traffics[i].size);
+                memset(expectedReadBuf.data(), traffics[i].byteVal, traffics[i].size);
+                clientStream.readback(readBuf.data(), traffics[i].size);
+                EXPECT_EQ(0, memcmp(readBuf.data(), expectedReadBuf.data(), traffics[i].size));
+                break;
+            }
+        }
+        clientStream.flush();
+    });
+
+    FunctorThread serverTestThread([&serverStream, &traffics]() {
+        std::vector<uint8_t> writeBuf;
+        std::vector<uint8_t> readBuf;
+        std::vector<uint8_t> expectedReadBuf;
+        for (uint32_t i = 0; i < traffics.size(); ++i) {
+            size_t wanted, read, writeSize;
+            switch (traffics[i].type) {
+            case TrafficType::Write:
+                wanted = traffics[i].size;
+                readBuf.resize(wanted);
+                expectedReadBuf.resize(wanted);
+                memset(expectedReadBuf.data(), traffics[i].byteVal, wanted);
+                read = 0;
+                while (read < wanted) {
+                    read += serverStream.read(readBuf.data() + read, wanted - read);
+                }
+                EXPECT_EQ(0, memcmp(readBuf.data(), expectedReadBuf.data(), wanted));
+                break;
+            case TrafficType::Read:
+                writeSize = traffics[i].size;
+                writeBuf.resize(writeSize);
+                memset(writeBuf.data(), traffics[i].byteVal, writeSize);
+                serverStream.writeFully(writeBuf.data(), traffics[i].size);
+                break;
+            }
         }
     });
 
